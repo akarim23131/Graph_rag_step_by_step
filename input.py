cdc5f64271792ab3a2d700e75b2d076e63a4997e
akarim23131 <129064416+akarim23131@users.noreply.github.com>
2024-11-13 14:18:19 +0500

    Update input.py

--- input.py
+++ input.py
@@ -4,4 +4,14 @@ import nest_asyncio
 import urllib.request
 nest_asyncio.apply()
 
-
+index_root = os.path.join(os.getcwd(), 'graph_index')
+os.makedirs(os.path.join(index_root, 'input'), exist_ok=True)
+url = "https://www.gutenberg.org/cache/epub/7785/pg7785.txt"
+file_path = os.path.join(index_root, 'input', 'davinci.txt')
+urllib.request.urlretrieve(url, file_path)
+with open(file_path, 'r+', encoding='utf-8') as file:
+    # If you want to save api key cost, you can truncate the text file to a smaller size.
+    lines = file.readlines()
+    file.seek(0)
+    file.writelines(lines[:934])  # Decrease this number if you want to save api key cost.
+    file.truncate()
